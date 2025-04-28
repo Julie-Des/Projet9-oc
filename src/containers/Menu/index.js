6660ba2823b8133130fc51fc7abb0e09b217bfc6
Julie-Des <julie.deshayes14@gmail.com>
2025-04-28 10:26:27 +0200

    fix: button contact works when clicked a second time

--- src/containers/Menu/index.js
+++ src/containers/Menu/index.js
@@ -4,24 +4,37 @@ import Logo from "../../components/Logo";
 
 import "./style.scss";
 
-const Menu = () => (
-  <nav>
-    <Logo />
-    <ul>
-      <li>
-        <a href="#nos-services">Nos services</a>
-      </li>
-      <li>
-        <a href="#nos-realisations">Nos réalisations</a>
-      </li>
-      <li>
-        <a href="#notre-equipe">Notre équipe</a>
-      </li>
-    </ul>
-    <Button title="contact" onClick={() => (window.document.location.hash = "#contact")}>
-      Contact
-    </Button>
-  </nav>
-);
+const Menu = () => {
+  const handleContactClick = () => {
+    if (window.location.hash === "#contact") {
+      window.location.hash = "";
+      setTimeout(() => {
+        window.location.hash = "#contact";
+      }, 0);
+    } else {
+      window.location.hash = "#contact";
+    }
+  };
+
+  return (
+    <nav>
+      <Logo />
+      <ul>
+        <li>
+          <a href="#nos-services">Nos services</a>
+        </li>
+        <li>
+          <a href="#nos-realisations">Nos réalisations</a>
+        </li>
+        <li>
+          <a href="#notre-equipe">Notre équipe</a>
+        </li>
+      </ul>
+      <Button title="contact" onClick={handleContactClick}>
+        Contact
+      </Button>
+    </nav>
+  );
+};
 
 export default Menu;
